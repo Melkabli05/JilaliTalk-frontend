3b8565eb86e2e5c7eeddc40d1a5597d0f2fe3d02
Mohammed El-kabli <melkabli05@gmail.com>
2026-07-07 23:48:51 +0100

    feat(messages): export MessageNewContactPanelComponent from feature index

--- src/app/features/messages/index.ts
+++ src/app/features/messages/index.ts
@@ -1,1 +1,2 @@
 export { MessagesPageComponent } from './pages/messages-page/messages-page';
+export { MessageNewContactPanelComponent } from './ui/new-contact-panel/messages-new-contact-panel.component';
